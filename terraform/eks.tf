9affefb1a64039652fe2ef223a37bcc5941612f5
janaa-wael <janawael119@gmail.com>
2026-09-06 17:40:47 +0300

    feat: configure Jenkins EKS client access

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -49,3 +49,12 @@ resource "aws_eks_cluster" "main" {
     Project = var.project_name
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "eks_api_from_jenkins" {
+  security_group_id            = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  referenced_security_group_id = aws_security_group.jenkins.id
+  description                  = "Allow Jenkins to reach the private EKS API endpoint"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+}
